c796dd8aa52e897e3b68b5645a515a131e15d7ab
minhnbnt <minhnbnt@gmail.com>
2023-03-12 09:44:18 +0700

    add title

--- code.ptit/C02031.c
+++ code.ptit/C02031.c
@@ -1,12 +1,36 @@
+/*
+Nhập vào chiều cao tam giác (không quá 20).
+
+Vẽ hình tam giác ký tự theo nguyên tắc tương ứng theo mẫu trong ví  dụ dưới đây.
+
+Input
+Chỉ có một số dòng ghi chiều cao (không quá 20).
+
+Output
+Ghi ra kết quả theo mẫu.
+
+Ví dụ
+
+Input:
+5
+
+Output:
+DEFGH
+CDEF
+BCD
+AB
+@
+*/
+
 #include <stdio.h>
 
 int main() {
-	int n,i,j;
-	scanf("%d",&n);
-	for(i=n-1; i>=0; i--) {
-		char c='@'+i;
-		for(j=0; j<=i; j++)
-			printf("%c",c+j);
+	int n, i, j;
+	scanf("%d", &n);
+	for (i = n - 1; i >= 0; i--) {
+		char c = '@' + i;
+		for (j = 0; j <= i; j++)
+			printf("%c", c + j);
 		printf("\n");
 	};
 	return 0;
